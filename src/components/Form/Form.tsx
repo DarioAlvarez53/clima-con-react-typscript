48268f0f9ca1b548da082141a27ef7d07010d8d7
Dario Alvarez <daralv53@gmail.com>
2024-09-20 12:16:49 -0600

    Insertando el formulario

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,31 @@
+import { countries } from "../../data/countries";
+
+export default function Form() {
+    return (
+        <form>
+            <div>
+                <label htmlFor="city">Ciudad:</label>
+                <input 
+                    id="city"
+                    type="text"
+                    name="city"
+                    placeholder="Ciudad"
+                />
+            </div>
+            <div>
+                <label htmlFor="city">Ciudad:</label>
+                <select>
+                    <option value="">-- Seleccione un País --</option>
+                    {countries.map(country => (
+                        <option 
+                            key={country.code}
+                            value={country.code}
+                        >{country.name}</option>
+                    ))}
+                </select>
+            </div>
+
+            <input type="submit" value='Consultar clima' />
+        </form>
+    )
+}
