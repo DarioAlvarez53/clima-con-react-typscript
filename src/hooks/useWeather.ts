c028123c121e92f612e5b9b237bdae9206ec4359
Dario Alvarez <daralv53@gmail.com>
2024-09-20 17:38:56 -0600

    Implementando valibot para respuesta

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { SearchType } from "../types";
-import { z } from "zod";
+// import { z } from "zod";
+import { object, string, number,InferOutput, parse } from "valibot"
 
 // function isWeatherResponse(weather: unknown) {
 //     return (
@@ -13,19 +14,29 @@ import { z } from "zod";
 //     )
 // }
 
-//Zod schema
-const Weather = z.object({
-    name: z.string(),
-    main: z.object({
-        temp: z.number(),
-        temp_max: z.number(),
-        temp_min: z.number()
+// //Zod schema
+// const Weather = z.object({
+//     name: z.string(),
+//     main: z.object({
+//         temp: z.number(),
+//         temp_max: z.number(),
+//         temp_min: z.number()
+//     })
+// })
+// //Creacion del type
+// type Weather = z.infer<typeof Weather>
+
+//Valibot schema
+const WeatherSchema = object({
+    name: string(),
+    main: object({
+        temp: number(),
+        temp_max: number(),
+        temp_min: number()
     })
 })
-
 //Creacion del type
-type Weather = z.infer<typeof Weather>
-
+type Weather = InferOutput<typeof WeatherSchema>
 
 export default function useWeather() {
 
@@ -60,15 +71,23 @@ export default function useWeather() {
             // const result = isWeatherResponse(weatherResult)
             // console.log(result);
             
-            //Implementando libreria Zod
+            // //Implementando libreria Zod
+            // const { data: weatherResult } = await axios(weatherUrl)
+            // const result = Weather.safeParse(weatherResult)
+            // if(result.success) {
+            //     console.log(result.data.name);
+            //     console.log(result.data.main.temp);
+            // }
+            
+            //Implementando libreria Valibot
             const { data: weatherResult } = await axios(weatherUrl)
-            const result = Weather.safeParse(weatherResult)
-            if(result.success) {
-                console.log(result.data.name);
-                console.log(result.data.main.temp);
+            const result = parse(WeatherSchema, weatherResult)
+            if(result) {
+                console.log(result.name);
+                console.log(result.main.temp);
+                
             }
             
-
 
         } catch (error) {
             console.log(error);
